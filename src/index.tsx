5c5fc434ee27bdc75949027bf8077c3f3b7210f4
Notryag <1135814285@qq.com>
2022-10-22 00:11:35 +0800

    stage2

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,18 @@
-import React from './zreact';
+import React from './zreact'
 
 function App() {
-    return (
-        <ul>
-            <li>li</li>
-            <li>li1</li>
-            <li>li2</li>
-            <li>li3</li>
-        </ul>
-    )
+  let count = 1
+  return (
+    <div>
+      <ul>
+        <li>li</li>
+        <li>li1</li>
+        <li>li2</li>
+        <li>li3</li>
+      </ul>
+      <h1>{count}</h1>
+    </div>
+  )
 }
 
-React.render(<App />, document.getElementById('root'));
+React.render(<App />, document.getElementById('root'))
--- src/zreact.js
+++ src/zreact.js
@@ -1,48 +1,127 @@
 function createElement(type, props = {}, ...children) {
-    console.log('%c [ children ]-2', 'font-size:13px; background:pink; color:#bf2c9f;', children)
-    return {
-        type,
-        props: {
-            ...props,
-            type,
-            children: children.map(child => typeof child === 'object' ? child : createTextElement(child))
-        }
-    }
-
+  return {
+    type,
+    props: {
+      ...props,
+      type,
+      children: children.map((child) => (typeof child === 'object' ? child : createTextElement(child))),
+    },
+  }
 }
 
 function createTextElement(text) {
-    return {
-        type: 'TEXT_ELEMENT',
-        props: {
-            nodeValue: text,
-            children: []
-        }
-    }
+  return {
+    type: 'TEXT_ELEMENT',
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  }
 }
 
+let wipRoot = null
+let nextUnitOfWork = null
 
 function render(element, container) {
-    if (typeof element.type === 'function') {
-        element = element.type(element.props)
-    }
-
-    const dom = element.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(element.type)
-    const isProperty = (p) => p !== 'children'
-
-    Object.keys(element.props).filter(isProperty).forEach(key => {
-        dom[key] = element.props[key]
-    })
-    
-    element.props.children.forEach(child => render(child, dom))
-
-    container.appendChild(dom)
-
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  }
+  nextUnitOfWork = wipRoot
 }
 
+function workLoop(deadline) {
+  let shouleYield = false
+  if (!shouleYield && nextUnitOfWork) {
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouleYield = deadline.timeRemaining() < 1
+  }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot(wipRoot.child)
+    wipRoot = null
+  }
+  window.requestIdleCallback(workLoop)
+}
+workLoop()
+
+function commitRoot(fiber) {
+  if (!fiber) return
+  let domParent = fiber.return
+  console.log('%c [ domParent ]-67', 'font-size:13px; background:pink; color:#bf2c9f;', domParent)
+
+  while (!domParent.dom) {
+    domParent = domParent.return
+  }
+  
+  if (fiber.dom && domParent.dom) {
+    domParent.dom.appendChild(fiber.dom)
+  }
+  commitRoot(fiber.child)
+  commitRoot(fiber.sibling)
+}
+
+function createDom(fiber) {
+  let dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type)
+      const isProperty = (p) => p !== 'children'
+    Object.keys(fiber.props)
+      .filter(isProperty)
+      .forEach((key) => {
+        dom[key] = fiber.props[key]
+      })
+  return dom
+}
+
+// 生成对应的fiber
+function performUnitOfWork(fiberNode) {
+  if (typeof fiberNode.type === 'function') {
+    reconcileChild(fiberNode, [fiberNode.type(fiberNode.props)])
+  } else {
+    if (!fiberNode.dom) {
+      fiberNode.dom = createDom(fiberNode)
+    }
+    reconcileChild(fiberNode, fiberNode.props.children)
+  }
+
+  if (fiberNode.child) {
+    return fiberNode.child
+  }
+
+  let nextFiber = fiberNode
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.return
+  }
+}
+
+function reconcileChild(fiber, children) {
+  let index = 0
+  let preFiber = null
+
+  while (index < children.length) {
+    let element = children[index]
+    let newFiber = {
+      type: element.type,
+      dom: null,
+      props: element.props,
+      return: fiber,
+    }
+    if (index === 0) {
+      fiber.child = newFiber
+    } else {
+      preFiber.sibling = newFiber
+    }
+    preFiber = newFiber
+
+    index++
+  }
+}
 
 export default {
-    createElement,
-    render,
-};
-
+  createElement,
+  render,
+}
